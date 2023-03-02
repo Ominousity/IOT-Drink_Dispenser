3c42d44bcd0500ccebfa2fba04a1dfdaa2672fb3
Ominousity <abs@hotmail.dk>
2023-03-02 14:52:45 +0100

    added togglebuttons, textfield and button

--- App/dispenser_app/lib/main.dart
+++ App/dispenser_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,18 +29,38 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  List<bool> isSelected = [false, true];
+  List<Widget> children = [Text("Drink"), Text("Shot")];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        children: [
+          Container(
+            color: Colors.black,
+            height: 49.4,
+            width: (MediaQuery.of(context).size.width),
+          ),
+          ToggleButtons(isSelected: isSelected, children: children, onPressed: (int index){
+            setState(() {
+                    for (int i = 0; i < isSelected.length; i++) {
+                      isSelected[i] = i == index;
+                    }
+                  });
+          }),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Centiliter'
+            ),
+          ),
+          ElevatedButton(onPressed: (){
+            
+          }, child: Text("Mix Drink!"))
+        ],
+      ),
     );
   }
 }
