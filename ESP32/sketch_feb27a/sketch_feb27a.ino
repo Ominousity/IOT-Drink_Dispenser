eda4c16a10ba32aea5c41510b264bb6b09a3926e
Zeroject <cypeyt@gmail.com>
2023-03-02 13:40:09 +0100

    Updated dispanse to error handle if data is too low

--- ESP32/sketch_feb27a/sketch_feb27a.ino
+++ ESP32/sketch_feb27a/sketch_feb27a.ino
@@ -1,6 +1,7 @@
 #include <WiFi.h>
 #include <Wire.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 int pump1 = 14;
 int pump2 = 13;
@@ -8,6 +9,11 @@ int button = 17;
 int redLight = 12;
 int yellowLight = 4;
 int greenLight = 16;
+double value1;
+double value2;
+
+TaskHandle_t OfflineDespense;
+StaticJsonBuffer<200> jsonBuffer;
 
 const char* ssid = "Jj 5G";
 const char* pass = "adhf0975";
@@ -30,9 +36,11 @@ void setup() {
   pinMode(yellowLight, OUTPUT);
   pinMode(greenLight, OUTPUT);  
   Serial.begin(9600);
+  digitalWrite(redLight, HIGH);
   setupWifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  xTaskCreatePinnedToCore(OfllineDespenseCode, "OfflineDespense", 10000, NULL, 1, &OfflineDespense, 1);
 }
 
 void setupWifi(){
@@ -44,7 +52,10 @@ void setupWifi(){
   WiFi.begin(ssid, pass);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    digitalWrite(redLight, HIGH);
+    delay(250);
+    digitalWrite(redLight, LOW);            
+    delay(250);
     Serial.print(".");
   }
 
@@ -58,30 +69,24 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
-  String messageTemp;
+  char messageTemp[255];
   
   for (int i = 0; i < length; i++) {
     Serial.print((char)message[i]);
-    messageTemp += (char)message[i];
+    messageTemp[i] = (char)message[i];
   }
   Serial.println();
-
   // Feel free to add more if statements to control more GPIOs with MQTT
-
+  JsonObject& root = jsonBuffer.parseObject(messageTemp);
+  if (!root.success()){
+    Serial.println("Failed to pass to Json");
+  }
+  value1 = root["value1"];
+  value2 = root["value2"];
   // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
   // Changes the output state according to the message
   if (String(topic) == "Bar/Tester") {
-    Serial.print("Changing output to ");
-    if(messageTemp == "on"){
-      testDrinkDespenser();      
-    }
-    else if(messageTemp == "off"){
-      Serial.println("off");
-      digitalWrite(pump1, HIGH);
-      digitalWrite(pump2, HIGH);
-      digitalWrite(greenLight, HIGH);
-      digitalWrite(yellowLight, LOW); 
-    }
+    testDrinkDespenser();      
   }
 }
 
@@ -90,7 +95,6 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     digitalWrite(redLight, HIGH);
-    digitalWrite(greenLight, LOW);
     // Attempt to connect
     client.setBufferSize(255);
     if (client.connect("mqtt-board-995a3702","po9JQvvuiSXMTzHnDhwmtNhRnpC3yXDNbAlPTE70lUC4h9fLeBVvd2cqgFjCsKdr", "po9JQvvuiSXMTzHnDhwmtNhRnpC3yXDNbAlPTE70lUC4h9fLeBVvd2cqgFjCsKdr")) {
@@ -100,6 +104,13 @@ void reconnect() {
       // Subscribe
       client.subscribe("Bar/Tester");
       client.setCallback(callback);
+      digitalWrite(yellowLight, HIGH);
+      delay(250);
+      digitalWrite(yellowLight, LOW);
+      delay(250);
+      digitalWrite(yellowLight, HIGH);
+      delay(250);
+      digitalWrite(yellowLight, LOW);      
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -108,16 +119,12 @@ void reconnect() {
       delay(5000);
     }
   }
-    
 }
 
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
-  if(digitalRead(button) == HIGH){
-    testDrinkDespenser();
-  }  
   client.loop();
 
   long now = millis();
@@ -130,22 +137,41 @@ void despenseDrink(){
   
 }
 
+void OfllineDespenseCode(void * parameter){
+  for(;;){
+    if (digitalRead(button)==HIGH){
+        testDrinkDespenser();
+      }
+    }
+}
+
 void testDrinkDespenser(){
   digitalWrite(greenLight, LOW);
-  digitalWrite(yellowLight, HIGH);
-  Serial.println("on");
-  digitalWrite(pump1, LOW);
-  digitalWrite(pump2, LOW);
-  delay(1000);
-  digitalWrite(pump1, HIGH);
-  delay(1000);
-  digitalWrite(pump1, LOW);
-  digitalWrite(pump2, HIGH);
-  delay(1000);
-  digitalWrite(pump2, LOW);
-  delay(1000);
-  digitalWrite(pump1, HIGH);
-  digitalWrite(pump2, HIGH);
-  digitalWrite(greenLight, HIGH);
-  digitalWrite(yellowLight, LOW);    
+  if (value1 <= 0.4 || value2 <= 0.4){
+    for (int i = 0; i <= 7; i++) {
+      digitalWrite(yellowLight, HIGH);
+      delay(250);
+      digitalWrite(yellowLight, LOW);
+      digitalWrite(redLight, HIGH);
+      delay(250);
+      digitalWrite(redLight, LOW);      
+    }
+  } else{
+    digitalWrite(yellowLight, HIGH);
+    Serial.println("on");
+    digitalWrite(pump1, LOW);
+    digitalWrite(pump2, LOW);
+    delay(value1 * 1000);
+    digitalWrite(pump1, HIGH);
+    delay(value2 * 1000);
+    digitalWrite(pump1, LOW);
+    digitalWrite(pump2, HIGH);
+    delay(value1 * 1000);
+    digitalWrite(pump2, LOW);
+    delay(value2 * 1000);
+    digitalWrite(pump1, HIGH);
+    digitalWrite(pump2, HIGH);
+    digitalWrite(greenLight, HIGH);
+    digitalWrite(yellowLight, LOW);    
+  }
 }
